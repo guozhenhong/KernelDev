3188a74197b11cce45a4e742cf9e421f8bbd1a24
guozhenhong <523890344@qq.com>
2014-03-26 11:20:44 +0800

    sv

--- helloTest/.tmp_versions/hellop.mod
+++
@@ -1,2 +0,0 @@
-/home/gzh/workspace/kernel_dev/helloTest/hellop.ko
-/home/gzh/workspace/kernel_dev/helloTest/hellop.o
--- scull/scull_unload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-module = "gzhSCULL"
-device = "gzhSCULL"
-
-/sbin/rmmod $module $* || exit 1
-
-rm -f /dev/${device} /dev/${device}[0-3]
